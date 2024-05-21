3f1ce98d202786cffabfe2c1abf385a1d50e88a7
Abigail Adegbiji <115902132+aabiji@users.noreply.github.com>
2024-05-21 21:05:22 +0000

    Respond with a list of found links

--- main.go
+++ main.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {}
+func respondWithError(err error, isServerError bool, w http.ResponseWriter) {
+	response := map[string]string{
+		"error": fmt.Sprintf("Internal error: %s", err),
+	}
+	encoded, _ := json.Marshal(response)
+	w.Write(encoded)
+
+	statusCode := 400
+	if isServerError {
+		statusCode = 500
+	}
+	w.WriteHeader(statusCode)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the query parameter from the request
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		respondWithError(err, true, w)
+		return
+	}
+
+	var values map[string]string
+	err = json.Unmarshal(body, &values)
+	if err != nil {
+		respondWithError(err, true, w)
+		return
+	}
+
+	query, ok := values["query"]
+	if !ok {
+		respondWithError(errors.New("query parameter not found"), false, w)
+		return
+	}
+
+	// Respond with links
+	links, err := GetVKDownloadLinks(query)
+	if err != nil {
+		respondWithError(err, true, w)
+		return
+	}
+
+	response := map[string][]string{"links": links}
+	encoded, err := json.Marshal(response)
+	if err != nil {
+		respondWithError(err, true, w)
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write(encoded)
+}
 
 func main() {
 	server := http.FileServer(http.Dir("web"))
--- web/index.js
+++ web/index.js
@@ -1,9 +1,20 @@
 
+function renderLinks(links) {
+    const div = document.getElementById("links");
+    for (let link of links) {
+        const a = document.createElement("a");
+        a.href = link;
+        a.innerHTML = "link"; // TODO: change this
+        div.appendChild(a);
+    }
+}
+
 // Call the /search endpoint on button click
 const button = document.getElementById("search");
 const query = document.getElementById("query");
 button.onclick = () => {
-    const url = "http://localhost:8080/search";
+    // TODO: change this back to localhost
+    const url = "https://cuddly-barnacle-45xjjqj5x763jr5r-8080.app.github.dev/search";
     const data = {
         method: "POST",
         body: JSON.stringify({query: query.value})
@@ -11,5 +22,13 @@ button.onclick = () => {
 
     fetch(url, data)
         .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then((json) => {
+            const errorMsg = document.getElementById("error");
+            if (json.error == undefined) {
+                errorMsg.innerHTML = "";
+                renderLinks(json.links);
+                return;
+            }
+            errorMsg.innerHTML = json.error;
+        });
 }
